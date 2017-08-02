aae33e936941c512f950e30d22d957b61143d2cd
marclura <marco@marcolurati.ch>
2017-08-02 14:31:07 +0200

    create the gulpfile.js and ignore node_modules

---
+++ dev/site/accounts/marcoadmin1.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: marcoadmin1
+password: >
+  $2a$10$sXgZgbXTZYcw3dY/bQPwDeHqE6PGygeiqnQ/3Tnkf4iaLRkEDI8AS
+email: marco@marcolurati.ch
+language: en
+role: admin
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,70 @@
 // Packages
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
     connect = require('gulp-connect-php');
 
+// Variables declaration
+var env,
+    jsSources,    // path to the js files
+    sassMainSource,   // the main.scss that @include all the other sass files
+    sassWatchSources,   // paths to the scss files
+    outputDir;    // define if it's the dev output or the distribution
+
+// Creting the environment
+env = process.env.NODE_ENV || 'dev';
+
+
+if (env==='dev') {
+  outputDir = 'dev/';
+} else {
+  outputDir = 'dist/';
+}
+
 // Sources
-var jsSources = [
-  'assets/js/*.js',
-  'assets/js/**/*.js'
+jsSources = [
+  'dev/assets/js/*.js',
+  'dev/assets/js/**/*.js'
 ];
 
-var saasSource = ['assets/sass/main.scss'];
+sassMainSource = [ 'dev/assets/sass/main.scss'];
+
+sassWatchSources = [
+  'dev/assets/sass/*.scss',
+  'dev/assets/sass/**/*.scss'
+];
 
 
 // Tasks
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
-    .pipe(gulp.dest('assets/builds'))
+    .pipe(gulp.dest(outputDir + 'assets/builds'))
 })
 
 
 gulp.task('sass', function() {
-  return gulp.src(saasSource)
+  return gulp.src(sassMainSource)
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename('style.css'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('assets/builds/'))
+    .pipe(gulp.dest(outputDir + 'assets/builds/'))
 })
 
 
 gulp.task('connect', function() {
-  connect.server({ port: 3000, keepalive: true });
+  connect.server({ base: outputDir, port: 3000, keepalive: true });
 })
 
 
 gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
-  gulp.watch(['assets/sass/*.scss', 'assets/sass/**/*.scss'], ['sass']);
+  gulp.watch(sassWatchSources, ['sass']);
 })
 
 
-// THis default task will be executed by just writing $ gulp
+// This default task will be executed by just writing $ gulp in the CLI
 gulp.task('default', ['connect', 'watch']);
