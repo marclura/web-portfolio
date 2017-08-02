98a6480ce3c497fe37426fda3173bde964e3bf2f
marclura <marco@marcolurati.ch>
2017-08-02 14:43:11 +0200

    remove dist folder

--- dist/site/accounts/marcoadmin1.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: marcoadmin1
-password: >
-  $2a$10$FHn4QfiGkIfDse4bhmiS6OlNaWaXsA.Z3FYnRbIvpWXWzbtgOhBTO
-email: marco@marcolurati.ch
-language: en
-role: admin
