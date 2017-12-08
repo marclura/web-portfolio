9418a9024ba5b13b7a9bb3fb4f08cacae80ad5fe
marclura <marco@marcolurati.ch>
2017-12-08 11:17:28 +0100

    gulp stylus and lost setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,60 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
+    less = require('gulp-less'),
+    stylus = require('gulp-stylus'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require('gulp-rename'),
-    connect = require('gulp-connect-php');
+    connect = require('gulp-connect-php'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    lost = require('lost'),
+    cssnano = require('cssnano');
 
 // Variables declaration
 var env,
     jsSources,    // path to the js files
     sassMainSource,   // the main.scss that @include all the other sass files
     sassWatchSources,   // paths to the scss files
-    outputDir;    // define if it's the dev output or the distribution
+    lessWatchSources,   // paths for the less files
+    stylusWatchSources,   // paths for the stylus files
+    outputDir,
+    cssOutputDir;    // define if it's the src output or the distribution
 
 // Creting the environment
-env = process.env.NODE_ENV || 'dev';
+env = process.env.NODE_ENV || 'src';
 
 
-if (env==='dev') {
-  outputDir = 'dev/';
+if (env==='src') {
+  outputDir = 'src/';
 } else {
   outputDir = 'dist/';
 }
 
+// css output path
+cssOutputDir = outputDir + 'assets/builds/';
+
 // Sources
 jsSources = [
-  'dev/assets/js/*.js',
-  'dev/assets/js/**/*.js'
+  'src/assets/js/*.js',
+  'src/assets/js/**/*.js'
 ];
 
-sassMainSource = [ 'dev/assets/sass/main.scss'];
+sassMainSource = [ 'src/assets/sass/main.scss'];
 
 sassWatchSources = [
-  'dev/assets/sass/*.scss',
-  'dev/assets/sass/**/*.scss'
+  'src/assets/sass/*.scss',
+  'src/assets/sass/**/*.scss'
+];
+
+lessWatchSources = [
+  'src/assets/less/*.less',
+  'src/assets(less/**/*.less'
+];
+
+stylusWatchSources = [
+  'src/assets/stylus/*.styl',
+  'src/assets/stylus/**/*.styl'
 ];
 
 
@@ -49,11 +71,39 @@ gulp.task('sass', function() {
   return gulp.src(sassMainSource)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(postcss([
+        lost(),
+        cssnano()
+      ]))
     .pipe(rename('style.css'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(outputDir + 'assets/builds/'))
+    .pipe(gulp.dest(cssOutputDir))
 })
 
+gulp.task('less', function() {
+  gulp.src(lessWatchSources)
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(cssOutputDir))
+})
+
+gulp.task('stylus', function() {
+  return gulp.src(stylusWatchSources)
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+
+    }))
+    .pipe(postcss([
+        lost(),
+        autoprefixer(),
+        cssnano()
+      ]))
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(cssOutputDir))
+})
 
 gulp.task('connect', function() {
   connect.server({ base: outputDir, port: 3000, keepalive: true });
@@ -62,9 +112,11 @@ gulp.task('connect', function() {
 
 gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
-  gulp.watch(sassWatchSources, ['sass']);
+  //gulp.watch(sassWatchSources, ['sass']);
+  //gulp.watch(lessWatchSources, ['less']);
+  gulp.watch(stylusWatchSources, ['stylus']);
 })
 
 
 // This default task will be executed by just writing $ gulp in the CLI
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'stylus', 'watch']);
